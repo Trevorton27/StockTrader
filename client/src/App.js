5aa68366ba366a13d859347d9bafb21aaf76b519
Trevor Mearns <spiral272@gmail.com>
2021-11-22 15:20:29 +0900

    added link component to header

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React, { useContext } from 'react';
-import { Route, BrowserRouter as Router, Redirect, Switch } from 'react-router-dom';
+import {
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+  Link
+} from 'react-router-dom';
 import AuthContext from './store/auth-context';
 import './App.css';
 import Header from './Components/Header';
 import Home from './Pages/Home';
 import Welcome from './Pages/Welcome';
 import Trade from './Pages/Trade';
-import Portfolio from './Pages/Portfolio'
+import Portfolio from './Pages/Portfolio';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -15,26 +21,18 @@ function App() {
 
   return (
     <Router>
-        <Header />
+      <Header />
 
       <Switch>
-          {!isLoggedIn && (
-            <Route exact path="/"  component={Home} />
-          )}
-          {isLoggedIn && (
-            <Route exact path="/"  component={Welcome} />
-          )}
-          {isLoggedIn && (
-            <Route path="/trade" component={Trade} />
-          )}
-          {isLoggedIn && (
-            <Route path="/portfolio" component={Portfolio} />
-          )}
-          <Route path="*">
-            <Redirect to="/" />
-          </Route>
-          {/* <Route path="*" component={Home} /> */}
-          {/* <Route render={() => <Redirect to={{path: "/"}} />} /> */}
+        {!isLoggedIn && <Route exact path='/' component={Home} />}
+        {isLoggedIn && <Route exact path='/' component={Welcome} />}
+        {isLoggedIn && <Route path='/trade' component={Trade} />}
+        {isLoggedIn && <Route path='/portfolio' component={Portfolio} />}
+        <Route path='*'>
+          <Redirect to='/' />
+        </Route>
+        {/* <Route path="*" component={Home} /> */}
+        {/* <Route render={() => <Redirect to={{path: "/"}} />} /> */}
       </Switch>
     </Router>
   );
--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -3,32 +3,36 @@ import { Nav, Navbar, Button } from 'react-bootstrap';
 import { useContext } from 'react';
 import AuthContext from '../store/auth-context';
 import UserContext from '../store/user-context';
-
+import { Link } from 'react-router-dom';
 function Header() {
-    const authCtx = useContext(AuthContext);
-    const userCtx = useContext(UserContext);
+  const authCtx = useContext(AuthContext);
+  const userCtx = useContext(UserContext);
 
-    const isLoggedIn = authCtx.isLoggedIn;
+  const isLoggedIn = authCtx.isLoggedIn;
 
-    function logoutHandler(){
-        authCtx.logout();
-        userCtx.logout();
-    };
+  function logoutHandler() {
+    authCtx.logout();
+    userCtx.logout();
+  }
 
-    return(
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
-            <Navbar.Brand href="#home"></Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav>
-                    <Nav.Link href="/">Home</Nav.Link>
-                    {isLoggedIn && (<Nav.Link href="/trade">Trade</Nav.Link>)}
-                    {isLoggedIn && (<Nav.Link href="/portfolio">Portfolio</Nav.Link>)}
-                    {isLoggedIn && (<Button variant="dark" onClick={logoutHandler}>Logout</Button>)}
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    );
-};
+  return (
+    <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark' sticky='top'>
+      <Navbar.Brand href='#home'></Navbar.Brand>
+      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+      <Navbar.Collapse id='responsive-navbar-nav'>
+        <Nav>
+          <Link to='/'>Home</Link>
+          {isLoggedIn && <Link to='/trade'>Trade</Link>}
+          {isLoggedIn && <Link to='/portfolio'>Portfolio</Link>}
+          {isLoggedIn && (
+            <Button variant='dark' onClick={logoutHandler}>
+              Logout
+            </Button>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
 
 export default Header;
