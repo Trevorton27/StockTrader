24ee0c90dcbc6c5114a42384ed97f305bf2c9309
Trevor Mearns <spiral272@gmail.com>
2022-01-06 20:05:12 +0900

    added jwt authentication

--- serverRoutes/authRoute.js
+++ serverRoutes/authRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database/db');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
   try {
@@ -51,10 +52,16 @@ router.get('/login', async (req, res) => {
     // }
 
     // res.json({ id: user.rows[0].user_id, name: user.rows[0].user_name });
-
+    const isAuthenticated = jwt.sign(user, process.env.TOKEN_SECRET_KEY, {
+      expiresIn: '3600000'
+    });
     const match = bcrypt.compare(password, user.rows[0].password);
     if (match) {
-      res.json({ id: user.rows[0].user_id, name: user.rows[0].user_name });
+      res.json({
+        id: user.rows[0].user_id,
+        name: user.rows[0].user_name,
+        authToken: isAuthenticated
+      });
     } else {
       return res.status(401).json('Invalid credentials.');
     }
